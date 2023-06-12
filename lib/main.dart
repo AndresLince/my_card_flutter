4ccf7a3f9b2ce40be3b35602ea520bc4d5bc0d90
AndresLince <lynxdarmatron@gmail.com>
2023-06-12 14:44:09 -0500

    Review hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Text('Hello world'),
-    )
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.red,
+            body: Center(
+              child: Text('Hello world'),
+            )
+        )
+    );
+  }
+
 }
