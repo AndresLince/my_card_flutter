59facc09bfb376f9430c4f06cdcfdcf2dacb19ea
AndresLince <lynxdarmatron@gmail.com>
2023-06-14 06:48:22 -0500

    flutter layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -4,39 +4,53 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
         home: Scaffold(
-            backgroundColor: Colors.teal,
-            body: SafeArea(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.white,
-                    child: const Text('Hello world'),
-                  ), Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.blue,
-                    child: const Text('Hello world'),
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.red,
-                    child: const Text('Hello world'),
-                  )
-                ],
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+
+                color: Colors.red,
               ),
             ),
-        )
-    );
+            const SizedBox(
+              width: 56,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: const Color(0xffCCAC00),
+                )
+              ],
+            ),
+            const SizedBox(
+              width: 56,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue
+            )
+          ],
+        ),
+      ),
+    ));
   }
-
 }
