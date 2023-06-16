50d1403bd476108fe2271acff2ea5a75f6da4507
AndresLince <lynxdarmatron@gmail.com>
2023-06-15 20:18:12 -0500

    Add image WIP

--- lib/main.dart
+++ lib/main.dart
@@ -8,45 +8,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(
+    return const MaterialApp(
         home: Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: Column(
           children: [
-            Expanded(
-              child: Container(
-
-                color: Colors.red,
-              ),
-            ),
-            const SizedBox(
-              width: 56,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color(0xffCCAC00),
-                )
-              ],
-            ),
-            const SizedBox(
-              width: 56,
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.blue
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.red,
             )
           ],
         ),
